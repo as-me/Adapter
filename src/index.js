4dc7a37e2c86249a390ed67790ae3d1afdf10839
Sanjay krishna anbalagan <sanjay1909@gmail.com>
2015-09-11 11:55:25 -0400

    available Charts are referenced in adapter.charts for use in external projects

--- src/index.js
+++ src/index.js
@@ -15,3 +15,13 @@ exports.components.D3.ScatterPlotTool = require("./lib/components/D3/ScatterPlot
 
 exports.components.C3 = {}
 exports.components.C3.ScatterPlotTool = require("./lib/components/C3/ScatterPlotTool.js");
+
+
+adapter.charts = {
+    d3: {
+        scatterplot: adapter.sessionTool.d3.ScatterPlotTool
+    },
+    c3: {
+        scatterplot: adapter.sessionTool.c3.ScatterPlotTool
+    }
+};
